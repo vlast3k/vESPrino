05c6ac21aeb87cd9562c8df4e7bc62eda0660feb
Vladi Vladi <vlast3k@gmail.com>
2016-12-26 18:55:04 +0200

    add support for post

--- tools/httpsrv.js
+++ tools/httpsrv.js
@@ -4,9 +4,16 @@ var http = require('http');
 //Lets define a port we want to listen to
 const PORT=8080; 
 
+  var ab2str = function(buf) {
+    var bufView = new Uint8Array(buf);
+    var encodedString = String.fromCharCode.apply(null, bufView);
+    return encodedString;
+  };
+
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    request.on('data', function (data) {  console.log(ab2str(data));  });
+    request.on('end', function () {   response.end('It Works!! Path Hit: ' + request.url);   });
     console.log(new Date().toLocaleTimeString() + ' - Requested: ' + request.url);
 }
 
@@ -15,6 +22,7 @@ var server = http.createServer(handleRequest);
 
 //Lets start our server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://192.168.1.116:%s", PORT);
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+        console.log("Server listening on: http://" + add +":%s", PORT);
+    })
 });
